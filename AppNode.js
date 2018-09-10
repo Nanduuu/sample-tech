4a4d743219d38b58b76ff95d8a645738c20ba5ae
Nanduuu <Nanduuu@users.noreply.github.com>
2018-09-10 07:34:14 +0530

    commit

--- AppNode.js
+++ AppNode.js
@@ -4,6 +4,9 @@ var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
 var routers = require('./email');
+var login = require('./login');
+var optcpt = require('./optcpt');
+var dailySenarios = require('./dailySenarios');
 
 var jsonParser  = bodyParser.json();
 var urlencodedparser = bodyParser.urlencoded({extended:true});
@@ -14,8 +17,14 @@ var urlencodedparser = bodyParser.urlencoded({extended:true});
 var port = process.env.PORT || 8080;
 
 var http= require('http');
-app.use(express.static('public/build'));
+//app.use(express.static('public/build'));
 app.use('/email/',routers);
+app.use('/login/',login);
+app.use('/optcpt/',optcpt);
+app.use('/dailySenarios/',dailySenarios);
+
+
+
 app.use(bodyParser.json());
 //app.use(urlencodedparser);
 
